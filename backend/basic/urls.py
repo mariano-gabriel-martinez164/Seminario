0d22538214cdbc353f21dbc9b570eea1c9a07232
LauAubert <aubertlautaroa@gmail.com>
2024-08-19 16:49:18 -0300

    fix: enpoints en plural

--- backend/basic/urls.py
+++ backend/basic/urls.py
@@ -2,15 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('centro/',views.CentrosOdontologicosList.as_view()),
-    path('centro/<int:pk>/',views.CentroOdontologicoDetail.as_view()),
+    path('centros/',views.CentrosOdontologicosList.as_view()),
+    path('centros/<int:pk>/',views.CentroOdontologicoDetail.as_view()),
 
-    path('odontologo/',views.OdontologosList .as_view()),
-    path('odontologo/<int:pk>/',views.OdontologoDetail.as_view()),
+    path('odontologos/',views.OdontologosList .as_view()),
+    path('odontologos/<int:pk>/',views.OdontologoDetail.as_view()),
 
-    path('piezaDental/',views.PiezasDentalesList.as_view()),
-    path('piezaDental/<int:pk>/',views.PiezaDentalDetail.as_view()),
+    path('piezasDentales/',views.PiezasDentalesList.as_view()),
+    path('piezasDentales/<int:pk>/',views.PiezaDentalDetail.as_view()),
 
-    path('prestacion/',views.PrestacionesList.as_view()),
-    path('prestacion/<int:pk>/',views.PrestacionDetail.as_view()),
+    path('prestaciones/',views.PrestacionesList.as_view()),
+    path('prestaciones/<int:pk>/',views.PrestacionDetail.as_view()),
 ]
