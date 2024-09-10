a49469e9356f9c75f1e9f09b4be6653ad89eb7b8
TomasMartin2004 <tomassmartin04@gmail.com>
2024-09-10 18:29:15 -0300

    feat: Gestionar turno terminado

--- frontend/Odontologia-CECQ/src/Components/GestionarUsuario/gestionarUsuario.jsx
+++ frontend/Odontologia-CECQ/src/Components/GestionarUsuario/gestionarUsuario.jsx
@@ -1,6 +1,4 @@
-import { Table } from 'react-bootstrap'
-import { Button, Container, Alert } from '@mui/material';
-import './gestionarUsuario.css'
+import { Button, Container, Alert, TableRow, TableHead, TableContainer, TableBody, Table, Paper, styled, TableCell, tableCellClasses } from '@mui/material';
 import { useEffect, useState } from 'react'
 import { apiUrl, token } from '../../Request/fetch.js';
 import { ModalModificarUsuario } from './Modal/modalModificarUsuario.jsx';
@@ -31,43 +29,66 @@ export default function GestionarUsuario() {
       setCreado(false);
       }, [estadoModal, creado]);
 
+      const StyledTableCell = styled(TableCell)(({ theme }) => ({
+        [`&.${tableCellClasses.head}`]: {
+          backgroundColor: theme.palette.common.black,
+          color: theme.palette.common.white,
+        },
+        [`&.${tableCellClasses.body}`]: {
+          fontSize: 14,
+        },
+      }));
+      
+      const StyledTableRow = styled(TableRow)(({ theme }) => ({
+        '&:nth-of-type(odd)': {
+          backgroundColor: theme.palette.action.hover,
+        },
+        '&:last-child td, &:last-child th': {
+          border: 0,
+        },
+      }));
+
   return (
-    <Container fixed id='container'>
+    <Container fixed sx={{ mt: 2 }}>
       {estadoModal === 'Eliminado' && <Alert severity="error" onClose={() => {setEstadoModal('')}}>Usuario eliminado</Alert>}
       {estadoModal === 'Modificado' && <Alert severity="warning" onClose={() => {setEstadoModal('')}}>Usuario modificado</Alert>}
       {estadoModal === 'Creado' && <Alert severity="success" onClose={() => {setEstadoModal('')}}>Usuario creado</Alert>}
       
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>Cuil</th>
-            <th>Email</th>
-            <th>Centro</th>
-            <th>Modificar</th>
-          </tr>
-        </thead>
-        <tbody>
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Nombre</StyledTableCell>
+            <StyledTableCell align="center">Apellido</StyledTableCell>
+            <StyledTableCell align="center">Email</StyledTableCell>
+            <StyledTableCell align="center">Cuil</StyledTableCell>
+            <StyledTableCell align="center">Agenda</StyledTableCell>
+            <StyledTableCell align="center">Opciones</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
           {administrativos.map((administrativo) => (
-            <tr key={administrativo.id}>
-              <td>{administrativo.first_name}</td>
-              <td>{administrativo.last_name}</td>
-              <td>{administrativo.cuil}</td>
-              <td>{administrativo.email}</td>
-              <td>{administrativo.centro}</td>
-              <td>
-              <Button onClick={() => {
-                setModalShowModificar(true);
-                setUsuarioSeleccionado(administrativo.id);
-              }} className='w-100' variant="contained"> Ver más...</Button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
+            <StyledTableRow key={administrativo.id}>
+              <StyledTableCell component="th" scope="row">
+                {administrativo.first_name}
+              </StyledTableCell>
+              <StyledTableCell align="center">{administrativo.last_name}</StyledTableCell>
+              <StyledTableCell align="center">{administrativo.email}</StyledTableCell>
+              <StyledTableCell align="center">{administrativo.cuil}</StyledTableCell>
+              <StyledTableCell align="center">{administrativo.centro}</StyledTableCell>
+              <StyledTableCell align="center">
+                <Button onClick={() => {
+                  setModalShowModificar(true);
+                  setUsuarioSeleccionado(administrativo.id);
+                }} className='w-100' variant="contained"> Opciones</Button>
+              </StyledTableCell>
 
+            </StyledTableRow>
+          ))}
+        </TableBody>
       </Table>
-      <Button onClick={() => setModalShowCrear(true)} variant="contained">
+     </TableContainer>
+      <Button onClick={() => setModalShowCrear(true)} sx={{ mt: 1 }} variant="contained">
         Crear usuario</Button>
 
       {modalShowModificar && (
