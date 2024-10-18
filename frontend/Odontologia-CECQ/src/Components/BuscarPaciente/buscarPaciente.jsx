c15829381e00c0ee2b9944e83d1c86a450811257
mariano martinez <martinez25mariano25@gmail.com>
2024-10-18 13:58:24 -0300

    feat: editar Paciente

--- frontend/Odontologia-CECQ/src/Components/BuscarPaciente/buscarPaciente.jsx
+++ frontend/Odontologia-CECQ/src/Components/BuscarPaciente/buscarPaciente.jsx
@@ -7,6 +7,8 @@ import { styled } from '@mui/material/styles';
 import  MostrarHistorias  from './historias.jsx';
 import { SelectorPacientes } from '../MaterialUI/selectores.jsx';
 import CrearPaciente from './crearPaciente.jsx';
+import EditarPaciente from './editarPaciente.jsx';
+
 
 export default function buscarPaciente() {
 	const [ paciente, cambiarPaciente ] = useState('');
@@ -77,73 +79,9 @@ export default function buscarPaciente() {
 						</Button>
 						<Button sx={{ position: 'absolute', left: 0, mt: '43vh', mr: 5 }} variant="contained" color="warning" onClick={abrirEditor}>
 							Editar paciente
-						</Button>
-					
-						<Modal open={abiertoE} onClose={cerrarEditor} >
-							<Box sx={{ 
-								position: 'absolute', 
-								top: '50%', 
-								left: '50%', 
-								transform: 'translate(-50%, -50%)',
-								backgroundColor: 'white',
-								p: 4,
-								m: 4,
-								borderRadius: 4,
-								boxShadow: 20,
-								width: 700
-							}}>
-								<form style={{ marginTop: 0 }}>
-									<Typography variant="h4" gutterBottom sx={{ mb: 2, textAlign: 'left' }}>
-										Editar datos 
-									</Typography>
-									<TextField 
-										sx={{ mt: 2 }}
-										id="standard-helperText" 
-										label="nuevo nombre" 
-										defaultValue={paciente.nombre} 
-										variant="standard" 
-										fullWidth />
-									<TextField 		
-										sx={{ mt: 2 }}
-										id="standard-helperText" 
-										label="nuevo apellido" 
-										defaultValue={paciente.apellido} 
-										variant="standard" 
-										fullWidth />
-									<TextField 		
-										sx={{ mt: 2 }}
-										id="standard-helperText" 
-										label="nuevo DNI" 
-										defaultValue={paciente.dni} 
-										variant="standard" 
-										fullWidth />
-									<TextField 		
-										sx={{ mt: 2 }}
-										id="standard-helperText" 
-										label="nuevo telefono" 
-										defaultValue={paciente.telefono} 
-										variant="standard" 
-										fullWidth />
-									<TextField 		
-										sx={{ mt: 2 }}
-										id="standard-helperText" 
-										label="nuevo correo" 
-										defaultValue={paciente.email} 
-										variant="standard" 
-										fullWidth />
-
-									<Stack direction="row" justifyContent="space-between" sx={{ mt: 5 }}>
-										<Button onClick={cerrarEditor}  variant="outlined" color="error">
-											Cancelar
-										</Button>
-										<Button type="submit" variant="contained" color="success">
-											Guardar
-										</Button>
-									</Stack>
-								</form>
-							</Box>
-						</Modal>
+						</Button>					
 						<CrearPaciente abrir={abiertoC} cerrar={cerrarCrear}/>
+						<EditarPaciente abrir={abiertoE} cerrar={cerrarEditor} paciente={paciente}/>
 				</Grid>
 					<Box sx={{ flexGrow: 1}}>
 						<MostrarHistorias paciente={paciente} />
