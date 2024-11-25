62531f367855e81b50e9a958f2788559c9fee194
LauAubert <aubertlautaroa@gmail.com>
2024-11-24 22:40:29 -0300

    fix: modelos en panel de administración de Django

--- backend/basic/admin.py
+++ backend/basic/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(CentroOdontologico)
+admin.site.register(Prestacion)
+admin.site.register(PiezaDental)
+admin.site.register(Odontologo)
