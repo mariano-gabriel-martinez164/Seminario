b1a3f61e2606befc5862fac59ab522192898443d
LauAubert <aubertlautaroa@gmail.com>
2025-09-17 20:51:41 -0300

    feat: cloud sql setup

--- backend/odontologia_cecq/settings.py
+++ backend/odontologia_cecq/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,8 +92,11 @@ WSGI_APPLICATION = 'odontologia_cecq.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'odontologia-db',
+        'PASSWORD': 'Admin.123',
+        'HOST': '34.176.247.2',
+        'PORT': '5432',
     }
 }
 
