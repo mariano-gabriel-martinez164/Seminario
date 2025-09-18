f7674acc6589d5cf3ddef10ff0be196821e026cf
TomasMartin2004 <tomas.martin@widergy.com>
2025-09-18 19:51:55 -0300

    fix: fixes

--- backend/odontologia_cecq/settings.py
+++ backend/odontologia_cecq/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=w!4)k&ltis(=!it8k#sx5ouhi*lv27h&j@k%ug*+70@&n*bif
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -137,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
