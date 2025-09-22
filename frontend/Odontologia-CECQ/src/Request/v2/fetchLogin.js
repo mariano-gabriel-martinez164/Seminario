a07596f87b25e02f04176437086a8313335acc8e
TomasMartin2004 <tomas.martin@widergy.com>
2025-09-21 22:15:53 -0300

    feat: chages

--- frontend/Odontologia-CECQ/src/Request/v2/fetchLogin.js
+++ frontend/Odontologia-CECQ/src/Request/v2/fetchLogin.js
@@ -1,27 +1,24 @@
+const apiUrl = import.meta.env.VITE_BASE_URL;
+
 export const fetchLogin = async (username, password, login, navigate) => {
-    try {
-      const response = await fetch('http://127.0.0.1:8000/auth/gettoken/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-      });
-  
-      if (response.ok) {
-        const data = await response.json();
-        const token = data.token;
-        login(token); 
-        navigate('/verAgenda'); 
-      } else {
-        return 'Error de autenticación';
-      }
-    } catch (error) {
-      return 'Error al realizar la solicitud';
+  try {
+    const response = await fetch(apiUrl + "/auth/gettoken/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      const token = data.token;
+      login(token);
+      navigate("/verAgenda");
+    } else {
+      return "Error de autenticación";
     }
-  };
-  
-  
-  
-  
-  
+  } catch (error) {
+    return "Error al realizar la solicitud";
+  }
+};
